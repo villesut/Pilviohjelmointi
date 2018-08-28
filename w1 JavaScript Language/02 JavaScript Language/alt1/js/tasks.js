db24d1e9f54cd4d2ec83b4a5fcd2365a5ed50aeb
Unknown <ville.sutinen@metropolia.fi>
2018-08-28 23:00:24 +0300

    jotain mukamas yritetty

--- w1 JavaScript Language/02 JavaScript Language/alt1/js/tasks.js
+++ w1 JavaScript Language/02 JavaScript Language/alt1/js/tasks.js
@@ -2,45 +2,57 @@
 // This script manages a to-do list.
 
 // Need a global variable:
-var tasks = []; 
+var tasks = [];
 
 // Function called when the form is submitted.
 // Function adds a task to the global array.
 function addTask() {
-    'use strict';
+	'use strict';
 
-    // Get the task:
-    var task = document.getElementById('task');
+	// Get the task:
+	var task = document.getElementById('task');
 
-    // Reference to where the output goes:
-    var output = document.getElementById('output');
-    
-    // For the output:
-    var message = '';
+	// Reference to where the output goes:
+	var output = document.getElementById('output');
 
-    if (task.value) {
-    
-        // Add the item to the array:
-        tasks.push(task.value);
-        
-        // Update the page:
-        message = '<h2>To-Do</h2><ol>';
-        for (var i = 0, count = tasks.length; i < count; i++) {
-            message += '<li>' + tasks[i] + '</li>';
-        }
-        message += '</ol>';
-        output.innerHTML = message;
-        
-    } // End of task.value IF.
+	// For the output:
+	var message = '';
 
-    // Return false to prevent submission:
-    return false;
-    
+	if (task.value) {
+		// Add the item to the array:
+		tasks.push(task.value);
+
+		// Update the page:
+		message = '<h2>To-Do</h2><ol>';
+		for (var i = 0, count = tasks.length; i < count; i++) {
+			message += '<li>' + tasks[i] + '</li>';
+		}
+		message += '</ol>';
+		output.innerHTML = message;
+	} // End of task.value IF.
+
+	// Return false to prevent submission:
+	return false;
 } // End of addTask() function.
+
+function removeDuplicates() {
+	var message = ' ';
+	var output = document.getElementById('output');
+
+	tasks = tasks.filter(function(item, index, inputArray) {
+		return inputArray.indexOf(item) == index;
+	});
+	message = '<h2>To-Do</h2><ol>';
+	for (var i = 0, count = tasks.length; i < count; i++) {
+		message += '<li>' + tasks[i] + '</li>';
+	}
+	message += '</ol>';
+	output.innerHTML = message;
+}
 
 // Initial setup:
 function init() {
-    'use strict';
-    document.getElementById('theForm').onsubmit = addTask;
+	'use strict';
+	document.getElementById('theForm').onsubmit = addTask;
 } // End of init() function.
 window.onload = init;
--- w1 JavaScript Language/02 JavaScript Language/alt2/js/docker.js
+++ w1 JavaScript Language/02 JavaScript Language/alt2/js/docker.js
@@ -1,34 +1,50 @@
 // koodi:
-var stone = new Stuff("stone", 3);
-var book = new Stuff("book", 7);
-var cotton = new Stuff("cotton", 0.001);
+
+class Bag {
+	constructor(maxWeight) {
+		this.items = [];
+		this.maxWeight = maxWeight;
+	}
+	add(item) {
+		this.items.push(item);
+	}
+	weight() {
+		this.maxWeight = this.maxWeight - item.weight;
+	}
+}
+
+class Cargo {
+	constructor(maxWeight) {}
+}
+
+var stone = new Stuff('stone', 3);
+var book = new Stuff('book', 7);
+var cotton = new Stuff('cotton', 0.001);
 
 var bag = new Bag(10);
 var vuitton = new Bag(3);
 
 var schenker = new Cargo(15);
 
-
 bag.add(stone);
-console.log("laukun paino, pitäisi olla 3: " + bag.weight());
+console.log('laukun paino, pitäisi olla 3: ' + bag.weight());
 bag.add(stone); // virhe: "Stuff lisätty jo, ei onnistu!"
 
 bag.add(book);
-console.log("laukun paino, pitäisi olla 10: " + bag.weight());
+console.log('laukun paino, pitäisi olla 10: ' + bag.weight());
 
 bag.add(cotton); // virhe: "Liian painava, ei pysty!"
 
-console.log("laukun paino, pitäisi olla 10: " + bag.weight());
-
+console.log('laukun paino, pitäisi olla 10: ' + bag.weight());
 
 schenker.add(bag);
 schenker.add(cotton); // virhe: Vääränlainen esine, ei onnistu!
 
-console.log("Ruuman paino, pitäisi olla 10: " + schenker.weight());
+console.log('Ruuman paino, pitäisi olla 10: ' + schenker.weight());
 
 vuitton.add(cotton);
 schenker.add(vuitton);
-console.log("Ruuman paino, pitäisi olla noin 10.001: " + schenker.weight()); 
+console.log('Ruuman paino, pitäisi olla noin 10.001: ' + schenker.weight());
 
 cotton.weight = 300;
-console.log("Ruuman paino, pitäisi olla 310: " + schenker.weight()); // hups!
+console.log('Ruuman paino, pitäisi olla 310: ' + schenker.weight()); // hups!
